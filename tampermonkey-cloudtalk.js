a685ced2ee62f561dd1b938a3139437cbe14f3e2
Niklas Vanhainen <niklasva82@users.noreply.github.com>
2021-08-05 00:01:03 +0200

    Create tampermonkey-cloudtalk.js

---
+++ tampermonkey-cloudtalk.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Zetkin CloudTalk link script
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Replace the tel: links in Zetkin with CloudTalk-compatible ct+tel://-links
+// @author       Niklas Vanhainen <niklas.vanhainen@zetkin.org>
+// @match        https://call.zetk.in/assignments/*/call
+// @icon         https://www.google.com/s2/favicons?domain=zetk.in
+// @grant        none
+// ==/UserScript==
+
+function editPhoneLink(element) {
+    if(element.href.startsWith('tel:')) {
+        console.log(element.href);
+        element.href = element.href.replace(/^tel\:/, 'ct+tel://');
+        console.log(element.href);
+    }
+}
+
+const selector = '.TargetInfo-number a';
+
+let el = document.querySelector(selector);
+if (el) {editPhoneLink(el)}
+new MutationObserver((mutationRecords, observer) => {
+    console.log(mutationRecords);
+    Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        editPhoneLink(element);
+    });
+}).observe(document.documentElement, {
+    childList: true,
+    subtree: true
+});
